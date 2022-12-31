2ba921c002e24c54ae4c940c22bc4aa03fed398f
Varun <aggarwalvarun58@gmail.com>
2022-12-31 19:20:39 +0530

    Added Templates for Pug

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
-	res.status(404).sendFile(path.join(rootDir, 'views/404.html'));
+	// res.status(404).sendFile(path.join(rootDir, 'views/404.html'));
+	res.render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const rootDir = require('../util/path');
 const path = require('path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+	res.render('shop', {
+		pageTitle: 'Shop',
+		path: '/',
+		prods: adminData.products,
+	});
+	// res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
